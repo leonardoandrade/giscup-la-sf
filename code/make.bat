6b92d4a3d382fc514609cb5d6c827f7a3bcf9094
sasfreitas <sasfreitas@gmail.com>
2012-07-19 16:01:02 +0000

    Windows Make Files
    git-svn-id: http://kimbiji.com/svn/giscup@9 af5a1163-16c8-4d05-a6b8-fa5af0d96cb9

---
+++ code/make.bat
@@ -0,0 +1,5 @@
+set LIB=c:\Program Files\Microsoft Visual Studio 9.0\VC\ATLMFC\LIB;c:\Program Files\Microsoft Visual Studio 9.0\VC\LIB;C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib
+
+nmake /f makefile.vc clean
+
+nmake /f makefile.vc all
---
+++ code/mapmatch.bat
@@ -0,0 +1,1 @@
+mapmatch.exe ./data/ ./input/ ./output/
